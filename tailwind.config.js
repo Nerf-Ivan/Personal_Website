192f6f0b153e285471f4e97c014318f61e48dd95
Ivan Swanepoel <ivan.swanepoel.dev@gmail.com>
2025-05-22 17:19:43 +0200

    Initial setup with flask and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html', // Scan all HTML files in templates folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
